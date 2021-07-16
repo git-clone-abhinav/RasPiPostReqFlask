5f2eb8fae5ebd47c4d4eae209d952978af919051
RaspberryPi <abhi.anu003@gmail.com>
2021-07-16 08:58:05 +0530

    Enhanced "/" Route

--- webserver.py
+++ webserver.py
@@ -2,9 +2,22 @@ from flask import Flask
 app = Flask(__name__)
 # import led
 import RPi.GPIO as GPIO
+import markdown
+import os
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.OUT)
+
+@app.route("/")
+def index():
+    """Some Documentation"""
+
+    # Open the ReadMe File
+    #with open(os.path.dirname(app.root_path) + '/README.md', 'r' ) as markdown_file:
+    with open('/home/pi/Desktop/RasPiPostReqFlask/README.md', 'r' ) as markdown_file:
+        content = markdown_file.read()
+
+        return markdown.markdown(content)
 
 @app.route("/on")
 def on():
